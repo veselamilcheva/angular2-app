46c2f703c747d290cb84654b4266ca0586f649d6
Vesela Milcheva <milchevavesela@gmail.com>
2018-04-25 17:20:45 -0400

    finishing up routing for the admin area

---
+++ src/app/admin/admin-shared/user.service.ts
@@ -0,0 +1,27 @@
+import {Injectable} from '@angular/core';
+import {
+    CanActivate,
+    Router,
+    ActivatedRouteSnapshot,
+    RouterStateSnapshot
+
+} from '@angular/router';
+
+@Injectable()
+export class UserService implements CanActivate {
+    userLoggedIn: boolean = false;
+
+    constructor (private router: Router) {}
+    canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): boolean {
+        const url: string = state.url;
+        return this.verifyLogin(url);
+    }
+
+    verifyLogin(url: string): boolean {
+        if (this.userLoggedIn) {
+            return true;
+        }
+        this.router.navigate(['admin/login']);
+        return false;
+    }
+}
--- src/app/admin/admin.module.ts
+++ src/app/admin/admin.module.ts
@@ -6,13 +6,16 @@ import { LoginComponent } from './loginComponent/login.component';
 import { AdminComponent } from './adminComponent/admin.component';
 import { AdminMenuComponent } from './admin-menu/admin-menu.component';
 
+import { UserService } from './admin-shared/user.service';
+
 const AdminRoutes: Routes = [
     {
         path: 'admin',
         component: AdminComponent,
         children: [
             { path: 'login', component: LoginComponent },
-            { path: '', component: AdminMenuComponent }
+            { path: 'signup', component: SignUpComponent },
+            { path: '', component: AdminMenuComponent, canActivate: [UserService] }
 
         ]
     }
@@ -28,42 +31,13 @@ const AdminRoutes: Routes = [
     declarations: [
         AdminComponent,
         AdminMenuComponent,
-        LoginComponent
+        LoginComponent,
+        SignUpComponent
+    ],
+    providers: [
+        UserService,
     ]
 })
 
 export class AdminModule {}
 
-
-// import { CommonModule } from '@angular/common';
-// import { RouterModule, Routes } from '@angular/router';
-// import { FormsModule } from '@angular/forms';
-
-// import { AdminComponent } from './adminComponent/admin.component';
-// import { AdminMenuComponent } from './adminMenu/admin-menu.component';
-// import { LoginComponent } from './login/login.component';
-// import { SignUpComponent } from './signUp/sign-up.component';
-
-// import { UserService } from './adminShared/user.service';
-
-// @NgModule({
-//     imports: [
-//         CommonModule,
-//         FormsModule,
-//         RouterModule.forChild(AdminRoutes)
-//     ],
-//     exports: [
-//         RouterModule
-//     ],
-//     declarations: [
-//         AdminComponent,
-//         AdminMenuComponent,
-//         LoginComponent,
-//         SignUpComponent
-//     ],
-//     providers: [
-//         UserService,
-//     ]
-// })
-
-// export class AdminModule {}
